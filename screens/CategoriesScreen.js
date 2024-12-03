911bae4abf0c31cea89996347a7eecd352cf2319
arushi <aroraarushi46@gmail.com>
2024-12-03 22:12:51 +0530

    Meals App

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,33 @@
+import {CATEGORIES} from '../data/dummy-data';
+import { FlatList } from 'react-native';
+import CategoryGridTitle from '../componnets/CategoryGridTile';
+
+
+function CategoriesScreen({navigation}){
+
+    function renderCategoryItem(itemData){
+        function pressHandler(){
+            navigation.navigate('MealsOverview',{
+                categoryId : itemData.item.id
+            })
+        }
+        return(
+            <CategoryGridTitle
+            title={itemData.item.title} 
+            color={itemData.item.color}
+            onPress={pressHandler} />
+        )
+    }
+
+   
+    
+    return(
+        <FlatList 
+        data={CATEGORIES}
+        keyExtractor={(item)=>item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}  />
+    )
+}
+
+export default CategoriesScreen
